5c4a0ac6e6df7c193165748119efbd0c600d1755
Chen Liang <liangchen76@gmail.com>
2024-12-31 13:17:13 -0600

    Used Claude on SOM with success

--- 6c.customer-service-REST/app-som.py
+++ 6c.customer-service-REST/app-som.py
@@ -16,6 +16,16 @@ gpt4o_llm_config = {
     "config_list": [{"model": "gpt-4o", "api_key": os.getenv("OPENAI_API_KEY")}],
 }
 
+claude_llm_config = {
+    "config_list": [
+        {
+            "model": "claude-3-5-sonnet-20240620",
+            "api_key": os.getenv("ANTHROPIC_API_KEY"),
+            "api_type": "anthropic",
+        }
+    ]
+}
+
 def termination_msg(x):
     return isinstance(x, dict) and "TERMINATE" == str(x.get("content", ""))[-9:].upper()
 
@@ -27,13 +37,13 @@ user_proxy = UserProxyAgent(
 
 product_manager = AssistantAgent(
     name="product manager",
-    llm_config=gpt4o_llm_config,
+    llm_config=claude_llm_config,
     system_message="You are a product manager. Come up with a product design to accomplish the user's request. You do not write code."
 )
 
 dev_manager = AssistantAgent(
     name="software development manager",
-    llm_config=gpt4o_llm_config,
+    llm_config=claude_llm_config,
     system_message="You are a software development manager. Rely the output of software_engineer and code_executor. You do not write code."
 )
 
@@ -54,7 +64,7 @@ When you find an answer, verify the answer carefully. Include verifiable evidenc
 
 software_engineer = AssistantAgent(
     name="Software Engineer",
-    llm_config=gpt4o_llm_config,
+    llm_config=claude_llm_config,
     system_message=code_writer_system_message,
 #     system_message="""Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
 # Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
@@ -94,14 +104,14 @@ dev_group_chat = GroupChat(
 
 dev_group_chat_manager = GroupChatManager(
     dev_group_chat, 
-    llm_config=gpt4o_llm_config,
+    llm_config=claude_llm_config,
     is_termination_msg=termination_msg,
 )
 
 society_of_mind_agent = SocietyOfMindAgent(
     "society_of_mind",
     chat_manager=dev_group_chat_manager,
-    llm_config=gpt4o_llm_config,
+    llm_config=claude_llm_config,
     is_termination_msg=termination_msg,
     default_auto_reply="Reply `TERMINATE` if the task is done.",
 )
